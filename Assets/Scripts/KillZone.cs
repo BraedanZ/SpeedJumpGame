cf41b178008b478eee2d0f2f589b017f5b63ab98
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2023-04-12 13:23:26 -0400

    remove debug logging

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -7,9 +7,7 @@ using UnityEngine.SceneManagement;
 public class KillZone : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other) {
-        print("Hello");
         if (other.CompareTag("Player")) {
-            print("hi");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
