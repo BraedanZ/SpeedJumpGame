7f0beee3ce3830b9e5ae62f9bd633859226f7552
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2023-05-18 17:06:19 -0400

    Difficulties can be set in the start method of gameMaster

--- Assets/Scripts/StaticClass.cs
+++ Assets/Scripts/StaticClass.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public static class StaticClass
 {
-    public static int difficulty;
+    private static int difficulty;
 
-    static void SetDifficulty(int difficulty) {
+    public static void SetDifficulty(int difficulty) {
         StaticClass.difficulty = difficulty;
     }
 
-    static int GetDifficulty() {
+    public static int GetDifficulty() {
         return StaticClass.difficulty;
     }
 }
