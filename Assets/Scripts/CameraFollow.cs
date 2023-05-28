ec0c0c38e076e1c6a461f0d58a1d1d2666baec79
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2023-05-28 16:17:41 -0400

    Use a gradient instead of lerping through colours

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,14 +13,11 @@ public class CameraFollow : MonoBehaviour
     private Color color1 = Color.blue;
     private Color color2 = Color.cyan;
 
-    private Color morningPink = new Color(.984f, .569f, .561f, 1f);
-    private Color morningTransition = new Color(.984f, .647f, .545f, 1f);
-    private Color morningOrange = new Color(1f, .831f, .592f, 1f);
-    private Color earlyBlue = new Color(.831f, .980f, 980f, 1f);
-    private Color midBlue = new Color(.765f, .933f, .980f, 1f);
-    private Color lateBlue = new Color(.686f, .835f, .941f, 1f);
+    public Gradient skyGradient;
 
     public Camera cam;
+
+    public float mapLength;
 
     public float colourModifierFromX;
 
@@ -46,12 +43,12 @@ public class CameraFollow : MonoBehaviour
     private void GradientBackground() {
         if (transform.position.x < 0) {
             colourModifierFromX = 0;
-        } else if (0 < transform.position.x && transform.position.x < 200) {
-            colourModifierFromX = transform.position.x / 200;
+        } else if (0 < transform.position.x && transform.position.x < mapLength) {
+            colourModifierFromX = transform.position.x / mapLength;
         } else {
             colourModifierFromX = 1;
         }
-        cam.backgroundColor = Color.Lerp(morningOrange, earlyBlue, colourModifierFromX);
+        cam.backgroundColor = skyGradient.Evaluate(colourModifierFromX);
     }
 
     private void SmoothCameraFollow() {
