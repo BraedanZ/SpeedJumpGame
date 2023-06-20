4dab49255d8521291dfeffe530ae208bc292f490
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2023-06-19 23:37:15 -0400

    start in full screen

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -35,7 +35,7 @@ public class SettingsMenu : MonoBehaviour
     }
 
     public void SetResolution (int resolutionIndex) {
-        Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = resolutions[resolutionIndex];   
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
