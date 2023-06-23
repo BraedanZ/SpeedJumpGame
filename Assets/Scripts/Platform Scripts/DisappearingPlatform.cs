48a894b31157dfd3467ba3d2b08ef2f66d94f824
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2023-06-23 01:53:13 -0400

    beautiful platforms added

--- Assets/Scripts/Platform Scripts/DisappearingPlatform.cs
+++ Assets/Scripts/Platform Scripts/DisappearingPlatform.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class DisappearingPlatform : MonoBehaviour
 {
+    private bool isActive;
+
+    private Renderer renderer;
+
+    // private Color fadedColour;
+    // private Color unFadedColour;
+
+    private Color lerpedColor = Color.white;
+
     void Start()
     {
+        renderer = this.gameObject.GetComponent<Renderer>();
         InvokeRepeating("ActivatePlatform", 2.0f, 4.0f);
         InvokeRepeating("DeactivatePlatform", 0.0f, 4.0f);
     }
 
+    void Update() {
+        // lerpedColor = Color.Lerp(new Color(255, 255, 255, 255), new Color(255, 255, 255, 0), Mathf.PingPong(Time.time, 1));
+        // renderer.material.color = lerpedColor;
+    }
+
     private void ActivatePlatform() {
+        isActive = true;
         this.gameObject.SetActive(true);
     }
 
     private void DeactivatePlatform() {
+        isActive = false;
         this.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Platform Scripts/ElectricPlatform.cs
+++ Assets/Scripts/Platform Scripts/ElectricPlatform.cs
@@ -6,11 +6,18 @@ public class ElectricPlatform : MonoBehaviour
 {
     public GameObject killZone;
 
+    private SpriteRenderer sprite;
+
+    private bool isFlipped;
+
     void Start()
     {
         killZone = this.gameObject.transform.GetChild(0).gameObject;
+        sprite = killZone.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>();
         InvokeRepeating("ActivateKillZone", 2.0f, 4.0f);
         InvokeRepeating("DeactivateKillZone", 0.0f, 4.0f);
+        InvokeRepeating("FlipLightning", 0.0f, 0.5f);
+
     }
 
     private void ActivateKillZone() {
@@ -19,5 +26,15 @@ public class ElectricPlatform : MonoBehaviour
 
     private void DeactivateKillZone() {
         killZone.SetActive(false);
+    }
+
+    private void FlipLightning() {
+        if (isFlipped) {
+            isFlipped = false;
+            sprite.flipX = false;
+        } else {
+            isFlipped = true;
+            sprite.flipX = true;
+        }
     }
 }
