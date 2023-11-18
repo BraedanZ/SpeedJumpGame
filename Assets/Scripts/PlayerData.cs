b9841b25c3bbccbfa4c704ba7ff50d31f8dc0c24
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2023-11-18 12:32:47 -0500

    level checkpoints work and fic background

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,45 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-[System.Serializable]
-public class PlayerData {
+// [System.Serializable]
+// public class PlayerData {
 
-    public int fallsInARow;
-    public float loadedTime;
-    public int deathCount;
-    public int jumpCount;
-    public float[,] checkpoints;
+//     public int fallsInARow;
+//     public float loadedTime;
+//     public int deathCount;
+//     public int jumpCount;
+//     public float[,] checkpoints;
 
-    public float[] position;
-    public float[] velocity;
+//     public float[] position;
+//     public float[] velocity;
 
-    public PlayerData (GameMaster gm) {
+//     public PlayerData (GameMaster gm) {
 
-        fallsInARow = gm.checkpointController.fallsInARow;
-        loadedTime = gm.elapsedTime;
-        deathCount = gm.deathCount;
-        jumpCount = gm.jumpCount;
+//         fallsInARow = gm.checkpointController.fallsInARow;
+//         loadedTime = gm.elapsedTime;
+//         deathCount = gm.deathCount;
+//         jumpCount = gm.jumpCount;
 
 
-        Stack<Vector2> copyOfReachedCheckPoints = new Stack<Vector2>(new Stack<Vector2>(gm.checkpointController.reachedCheckPoints));
+//         Stack<Vector2> copyOfReachedCheckPoints = new Stack<Vector2>(new Stack<Vector2>(gm.checkpointController.reachedCheckPoints));
 
-        checkpoints = new float[copyOfReachedCheckPoints.Count, 2];
-        for (int i = copyOfReachedCheckPoints.Count - 1; i >= 0; i--) {
-            Vector2 checkpoint = copyOfReachedCheckPoints.Pop();
-            checkpoints[i, 0] = checkpoint.x;
-            checkpoints[i, 1] = checkpoint.y;
-        }
+//         checkpoints = new float[copyOfReachedCheckPoints.Count, 2];
+//         for (int i = copyOfReachedCheckPoints.Count - 1; i >= 0; i--) {
+//             Vector2 checkpoint = copyOfReachedCheckPoints.Pop();
+//             checkpoints[i, 0] = checkpoint.x;
+//             checkpoints[i, 1] = checkpoint.y;
+//         }
 
-        position = new float[3];
-        Vector3 playerPosition = gm.player.GetPosition();
-        position[0] = playerPosition.x;
-        position[1] = playerPosition.y;
-        position[2] = playerPosition.z;
+//         position = new float[3];
+//         Vector3 playerPosition = gm.player.GetPosition();
+//         position[0] = playerPosition.x;
+//         position[1] = playerPosition.y;
+//         position[2] = playerPosition.z;
 
-        velocity = new float[2];
-        Vector2 playerVelocity = gm.player.GetVelocity();
-        velocity[0] = playerVelocity.x;
-        velocity[1] = playerVelocity.y;
-    }
-}
+//         velocity = new float[2];
+//         Vector2 playerVelocity = gm.player.GetVelocity();
+//         velocity[0] = playerVelocity.x;
+//         velocity[1] = playerVelocity.y;
+//     }
+// }
