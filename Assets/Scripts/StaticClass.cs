458f50dbe012f5ffe0fe097f5b438cde429f10a6
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2025-06-16 17:40:02 -0400

    world and level variable, setter, getter added to static class

--- Assets/Scripts/StaticClass.cs
+++ Assets/Scripts/StaticClass.cs
@@ -6,11 +6,36 @@ public static class StaticClass
 {
     private static int difficulty = 4;
 
-    public static void SetDifficulty(int difficulty) {
+    private static int world = 1;
+
+    private static int level = 1;
+
+    public static void SetDifficulty(int difficulty)
+    {
         StaticClass.difficulty = difficulty;
     }
 
-    public static int GetDifficulty() {
+    public static int GetDifficulty()
+    {
         return StaticClass.difficulty;
+    }
+
+    public static void SetWorld(int world)
+    {
+        StaticClass.world = world;
+    }
+
+    public static int GetWorld()
+    {
+        return StaticClass.world;
+    }
+    
+    public static void SetLevel(int level)
+    {
+        StaticClass.level = level;
+    }
+
+    public static int GetLevel() {
+        return StaticClass.level;
     }
 }
