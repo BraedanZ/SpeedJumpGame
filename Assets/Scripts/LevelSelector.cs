db357f6fe4bdee8eaaad03f85b48e4d0639211b4
BraedanZ <60045806+BraedanZ@users.noreply.github.com>
2025-06-16 15:27:45 -0400

    world and level menues entirely reworked

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -8,17 +8,29 @@ public class LevelSelector : MonoBehaviour
 {
     public static int selectedLevel = 1;
     public int level;
+
+    public int world;
     public Text levelText;
 
     void Start() {
         levelText.text = level.ToString();
     }
 
+    public void SetWorld(int world)
+    {
+        this.world = world; 
+    }
+
+    public void SetLevel(int level)
+    {
+        this.level = level; 
+    }
+
     public void OpenScene()
     {
         selectedLevel = level;
         // SceneManager.LoadScene("SampleLevel 1");
-        SceneManager.LoadScene("Assets/Scenes/Worlds/1/1-" + level + ".unity");
+        SceneManager.LoadScene("Assets/Scenes/Worlds/" + world + "/" + world + "-" + level + ".unity");
 
     }
 }
